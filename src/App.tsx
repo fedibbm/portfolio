c82566a857fc29b4ebce90d086f50691f5ba61b8
fedibbm <benbrahimfedi4@gmail.com>
2025-07-06 16:59:20 +0100

    previous commit patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-
 // import ScrollProgressBar from './components/ScrollProgressBar'
-import HomePage from './pages/HomePage'
-import Container from './Container'
+import HomePage from "./pages/HomePage";
+import Container from "./Container";
+import ClickSpark from "./blocks/Animations/ClickSpark/ClickSpark";
 
 function App() {
-
-  return (
-      <Container>
-        {/* <ScrollProgressBar /> */}
-        <HomePage />
-      </Container>
-  )
+    return (
+        <ClickSpark
+            sparkColor="#fff"
+            sparkSize={10}
+            sparkRadius={15}
+            sparkCount={8}
+            duration={400}
+        >
+            <Container>
+                {/* <ScrollProgressBar /> */}
+                <HomePage />
+            </Container>
+        </ClickSpark>
+    );
 }
 
-export default App
+export default App;
